ba3ad971537966177589b7d8010ee72cfc5a061e
lutheranders1 <lutheranders@gmail.com>
2022-04-11 11:33:54 -0500

    commit

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ const getUserProfile = async (req, res) => {
   try {
     const user = await (
       await User.findById(req.currentUser._id)
-    ).populate("createdMovies");
+    ).populate("postedBy");
     if (!user) throw new Error();
     return res.status(200).json(user);
   } catch (err) {
